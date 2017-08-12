65bec723388f3902228b06eefe8a7b5d4d3c05ae
Trenton Maki <trentonmaki@gmail.com>
2017-08-11 20:01:09 -0500

    Pre-deployment save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CrawlerComponent } from './crawler/crawler.component';
+import {environment} from "../environments/environment";
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'crawl/:hostname',
+    component: CrawlerComponent,
+    data: {title: 'Results'}
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    data: {title: ''}
+  },
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    CrawlerComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: environment.production} // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
